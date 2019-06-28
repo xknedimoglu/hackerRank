886e156cb0f01b1195198ef661db15d376468f77
xknedimoglu <46479100+xknedimoglu@users.noreply.github.com>
2019-06-28 17:24:37 +0300

    Update time-conversion-EnglishSolution.java

--- Java 'Time Conversion' Example Algorithm And Solution/time-conversion-EnglishSolution.java
+++ Java 'Time Conversion' Example Algorithm And Solution/time-conversion-EnglishSolution.java
@@ -1,3 +1,6 @@
+@author Rüştü Kaan Nedimoğlu
+@date 28.06.2019
+    
 import java.io.*;
 import java.math.*;
 import java.text.*;
@@ -5,12 +8,8 @@ import java.util.*;
 import java.util.regex.*;
 
 public class Solution {
-
-    /*
-     * Complete the timeConversion function below.
-     */
     static String timeConversion(String s) {
-        int s2=0;
+         int s2=0;
          String [] s3 = new String[3];
          String [] s4 = new String[3];
          s3 = s.split(":"); 
@@ -24,33 +23,31 @@ public class Solution {
          if(Integer.parseInt(s.substring(0,2))<12 && s.contains("AM")){   
             s4[2] = s3[2].substring(0,2);
             return s3[0]+":"+s3[1]+":"+s4[2];
-        }
-     
+        }     
        else if(Integer.parseInt(s.substring(0,2))==12 && s.contains("AM")){
            s2=Integer.parseInt(s.substring(0,2));
           s3[0]= Integer.toString(s2);
             s3[2] = s3[2].substring(0,2);
             return "00"+":"+s3[1]+":"+s3[2];
        }
-    
         else {
             s3[2] = s3[2].substring(0,2);
             return "12"+":"+s3[1]+":"+s3[2];
         }
     }
 
-    private static final Scanner scan = new Scanner(System.in);
+//  private static final Scanner scan = new Scanner(System.in);
 
     public static void main(String[] args) throws IOException {
-        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+//        BufferedWriter bw = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
 
-        String s = scan.nextLine();
+//        String s = scan.nextLine();
 
-        String result = timeConversion(s);
+//        String result = timeConversion(s);
 
-        bw.write(result);
-        bw.newLine();
+//        bw.write(result);
+//        bw.newLine();
 
-        bw.close();
+ //       bw.close();
     }
 }
