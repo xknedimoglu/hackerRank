bf307ee1b798acbf148f063da38b4e67c5daaf95
xknedimoglu <46479100+xknedimoglu@users.noreply.github.com>
2019-06-28 17:13:25 +0300

    Update bon-appetit-EnglishSolution.java

--- Java 'Bon Appetit' Example Algorithm And Solution/bon-appetit-EnglishSolution.java
+++ Java 'Bon Appetit' Example Algorithm And Solution/bon-appetit-EnglishSolution.java
@@ -1,3 +1,6 @@
+@author Rüştü Kaan Nedimoğlu
+@date   28.06.2019
+
 import java.io.*;
 import java.math.*;
 import java.security.*;
@@ -7,8 +10,6 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 public class Solution {
-
-    // Complete the bonAppetit function below.
     static void bonAppetit(List<Integer> bill, int k, int b) {
         int sum=0;
         int billK=0;
@@ -20,41 +21,37 @@ public class Solution {
                     sum = sum + bill.get(i);
                 }
             }
-            sum = sum /2;
+            sum = sum / 2;
             if(sum==b){
                 System.out.println("Bon Appetit");
             }
             else if(b>=sum){
                 System.out.println(b-sum);
-
             }
-        
-
-
     }
 
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+ //        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] nk = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+ //       String[] nk = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
-        int n = Integer.parseInt(nk[0]);
+ //      int n = Integer.parseInt(nk[0]);
 
-        int k = Integer.parseInt(nk[1]);
+ //       int k = Integer.parseInt(nk[1]);
 
-        String[] billItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+ //       String[] billItems = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
 
-        List<Integer> bill = new ArrayList<>();
+ //     List<Integer> bill = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            int billItem = Integer.parseInt(billItems[i]);
-            bill.add(billItem);
-        }
+ //      for (int i = 0; i < n; i++) {
+ //           int billItem = Integer.parseInt(billItems[i]);
+ //           bill.add(billItem);
+ //       }
 
-        int b = Integer.parseInt(bufferedReader.readLine().trim());
+ //       int b = Integer.parseInt(bufferedReader.readLine().trim());
 
-        bonAppetit(bill, k, b);
+ //       bonAppetit(bill, k, b);
 
-        bufferedReader.close();
-    }
+ //       bufferedReader.close();
+  //  }
 }
