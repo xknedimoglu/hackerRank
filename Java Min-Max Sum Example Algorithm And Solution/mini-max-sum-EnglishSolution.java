525c0176cbbc15f9f7f4b897e27ba352f13eb32f
xknedimoglu <46479100+xknedimoglu@users.noreply.github.com>
2019-07-01 17:16:50 +0300

    Add files via upload

---
+++ Java Min-Max Sum Example Algorithm And Solution/mini-max-sum-EnglishSolution.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static void miniMaxSum(int[] arr) {
+
+            Arrays.sort(arr);
+            long minSum = 0;
+            long maxSum = 0;
+            long sum = 0;
+
+            for(int i = 1 ; i< arr.length-1; i++){
+               sum = sum + arr[i];
+            }
+            minSum = sum + arr[0];
+            maxSum=  sum + arr[arr.length-1];
+
+            System.out.println(minSum + " " + maxSum);
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < 5; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        miniMaxSum(arr);
+
+        scanner.close();
+    }
+}
