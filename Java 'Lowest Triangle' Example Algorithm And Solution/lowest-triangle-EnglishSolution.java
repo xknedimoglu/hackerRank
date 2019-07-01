27b97e95123b0585d0b8999bb458131933fbda4a
xknedimoglu <46479100+xknedimoglu@users.noreply.github.com>
2019-07-01 17:19:53 +0300

    Add files via upload

---
+++ Java 'Lowest Triangle' Example Algorithm And Solution/lowest-triangle-EnglishSolution.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int lowestTriangle(int base, int area){
+        float height = ( area * 2 ) / base;
+        if((area*2) % base != 0 ){
+            height = height + 1;
+        }
+        int value = Math.round(height);
+        return value;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int base = in.nextInt();
+        int area = in.nextInt();
+        int height = lowestTriangle(base, area);
+        System.out.println(height);
+    }
+}
+
