69d304a300437469ee0e27e4c2b4cbb1f755df48
Kaan Nedimoğlu <46479100+xknedimoglu@users.noreply.github.com>
2019-07-06 11:43:49 +0300

    Add files via upload

---
+++ Java End-Of-File Example Algorithm And Solution/java-end-of-file-EnglishSolution.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+
+        int i = 1;
+        while(scn.hasNext()){
+            System.out.println(i + " " + scn.nextLine());
+            i++;
+        }
+    }
+}
