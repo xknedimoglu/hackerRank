5651e5b2b8fe7b9166fc743b4d5c396dde842d64
Kaan Nedimoğlu <46479100+xknedimoglu@users.noreply.github.com>
2019-07-06 11:44:38 +0300

    Update java-end-of-file-EnglishSolution.java

--- Java End-Of-File Example Algorithm And Solution/java-end-of-file-EnglishSolution.java
+++ Java End-Of-File Example Algorithm And Solution/java-end-of-file-EnglishSolution.java
@@ -1,3 +1,6 @@
+@author Rüştü Kaan Nedimoğlu
+@date   06.07.2019
+    
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -7,9 +10,10 @@ import java.util.regex.*;
 public class Solution {
 
     public static void main(String[] args) {
+        
         Scanner scn = new Scanner(System.in);
-
         int i = 1;
+        
         while(scn.hasNext()){
             System.out.println(i + " " + scn.nextLine());
             i++;
